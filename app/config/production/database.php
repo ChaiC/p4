79dfd2dcd908823771727f9c0fb940ba9fa5ee2e
Chaitanya Chitale <chaitanya.chitale@gmail.com>
2014-08-07 11:51:03 -0400

    p4V2

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => '127.5.210.2',
+            'database'  => 'surveyllance',
+            'username'  => 'admindEwshCu',
+            'password'  => 'U2JuiKlHlYLZ',
+        ),
+
+    ),
+
+);
